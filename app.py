a9c3035533975efed4cb49cebde0481bc8f294fb
Uday Kiran Tentu <uktentu@gmail.com>
2024-04-23 21:47:58 +0530

    S Commit

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+
